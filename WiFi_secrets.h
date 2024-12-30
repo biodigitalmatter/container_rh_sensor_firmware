585d643e105066aec20073f73c8d53cdc8e9baf8
Anton Tetov <anton@tetov.se>
2024-12-30 13:40:35 +0100

    wip

---
+++ WiFi_secrets.h
@@ -0,0 +1,8 @@
+#ifndef WIFI_SECRETS_H_
+#define WIFI_SECRETS_H_
+
+char* WIFI_SSID = "";
+char* WIFI_PASSWORD = ""
+
+
+#endif // WIFI_SECRETS_H_
--- container_rh_sensor_firmware.ino
+++ container_rh_sensor_firmware.ino
@@ -2,6 +2,11 @@
 #include <ArduinoJson.h>
 #include <M5Stack.h>
 #include <M5UnitENV.h>
+#include <WiFi.h>
+#include "WiFi_secrets.h" // WIFI_SSID & WIFI_PASSWORD
+
+const uint16_t WIFI_CONN_TRIES = 8;
+const char[] WIFI_HOSTNAME = "container_rh_sensor";
 
 SCD4X scd4x;
 JsonDocument doc;
@@ -10,14 +15,46 @@ uint16_t CO2_PPM;
 float TEMPERATURE_C;
 float HUMIDITY_PERCENT;
 
+void initWiFi(char[] hostname, char[] ssid, char[] pwd, int conn_tries=5) {
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname(hostname);
+  WiFi.begin(ssid, pwd);
+  Serial.print("Connecting to WiFi using ");
+  Serial.print(conn_tries);
+  Serial.println(" tries");
+  for (int i = 0; i < conn_tries; i++) {
+    Serial.print("Connecting try ");
+    Serial.println(i);
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.print("Connected with IP: ");
+      Serial.println(WiFi.localIP());
+      return true;
+      }
+    delay(1000);
+  }
+  Serial.println("Failed to connect. Continuing.")
+  return false;
+}
+
+void displayReading(char[] desc, String val) {
+    M5.Lcd.setTextSize(5);
+    M5.Lcd.println(desc);
+    M5.Lcd.setTextSize(7);
+    M5.Lcd.println(val);
+}
+
 void setup() {
-  doc["sensor"] = "container_rh";
   Serial.begin(115200);
+
+  doc["sensor"] = "container_rh";
 
   M5.begin();
   M5.Power.begin();
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextSize(7);
+
+  initWiFi(WIFI_HOSTNAME, WIFI_SSID, WIFI_PASSWORD, WIFI_CONN_TRIES);
 
   if (!scd4x.begin(&Wire, SCD4X_I2C_ADDR, 21, 22, 400000U)) {
     Serial.println("Couldn't find SCD4X");
@@ -55,22 +92,24 @@ void loop() {
     M5.Lcd.fillScreen(BLACK);
     M5.Lcd.println();
 
+    displayReading("T(C):", String(TEMPERATURE_C));
+    displayReading("RH%:", String(HUMIDITY_PERCENT));
 
-    M5.Lcd.setTextSize(5);
-    M5.Lcd.println(F("T(C):"));
-    M5.Lcd.setTextSize(7);
-    M5.Lcd.println(TEMPERATURE_C, 1);
+    char[] ip;
 
-    M5.Lcd.setTextSize(5);
-    M5.Lcd.println(F("RH%:"));
-    M5.Lcd.setTextSize(7);
-    M5.Lcd.println(HUMIDITY_PERCENT, 1);
+    if (WiFi.status() != WL_CONNECTED) {
+      ip = WiFi.localIP();
+    } else {
+      ip = "Not connected";
+    }
+
+    displayReading("IP:", String(ip));
+
     M5.Lcd.setCursor(0, 0);
 
-    Serial.println();
   } else {
     M5.Lcd.setTextSize(5);
-    M5.Lcd.print(F("."));
+    M5.Lcd.print(".");
   }
 
   delay(1000);
